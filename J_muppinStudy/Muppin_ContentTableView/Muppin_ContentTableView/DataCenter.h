8f28af5df038ba2d0542d13d7e5e7e2c38afe295
Hanson Jung <hansonjung@gmail.com>
2017-03-14 23:44:07 +0900

    글 길이에 따라 유동적으로 변하게. label의 text를 set한 뒤에 sizeToFit이 나와야 fit이 가능함..을 찾느라...

---
+++ J_muppinStudy/Muppin_ContentTableView/Muppin_ContentTableView/DataCenter.h
@@ -0,0 +1,13 @@
+//
+//  DataCenter.h
+//  Muppin_ContentTableView
+//
+//  Created by Hanson Jung on 2017. 3. 14..
+//  Copyright © 2017년 waaoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataCenter : NSObject
+
+@end
---
+++ J_muppinStudy/Muppin_ContentTableView/Muppin_ContentTableView/DataCenter.m
@@ -0,0 +1,13 @@
+//
+//  DataCenter.m
+//  Muppin_ContentTableView
+//
+//  Created by Hanson Jung on 2017. 3. 14..
+//  Copyright © 2017년 waaoo. All rights reserved.
+//
+
+#import "DataCenter.h"
+
+@implementation DataCenter
+
+@end
